a5166493957e5c64f6c3ba6252c412f66ec69902
Husnoro <Husnoro>
2020-04-20 14:14:07 +0500

    add user choose

--- culc/Program.cs
+++ culc/Program.cs
@@ -4,15 +4,30 @@ namespace culc
 {
     class Program
     {
-        public T Plus<T>(T First, T Second)
+        public static T Plus<T>(T First, T Second)
         {
             return (dynamic)First + (dynamic)Second; 
         }
-        
+        public static T Minus<T>(T First, T Second)
+        {
+            return (dynamic)First - (dynamic)Second; 
+        }
         static void Main(string[] args)
         {
-            
-            Console.WriteLine();
+            System.Console.WriteLine("Choose operation:\nPush *1* --> to plus operands\nPush *2* --> to minus operands\n Push *3* to find multyplication\nPush *4* --> to divide them\n Your choise:");
+            int choise = int.Parse(Console.ReadLine());
+            switch (choise)
+            {
+                case 1:
+                break;
+                case 2:
+                break;
+                case 3:
+                break;
+                case 4:
+                break;
+            }
+           
         }
     }
 }
