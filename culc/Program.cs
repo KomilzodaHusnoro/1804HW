b28e8abb626e74dda87533e57b56af022be51010
Husnoro <Husnoro>
2020-04-20 15:41:01 +0500

    упаковала всё

--- culc/Program.cs
+++ culc/Program.cs
@@ -26,15 +26,14 @@ namespace culc
         delegate T Rezult<T>(T First, T Second);
         static void Main(string[] args)
         {
-            //System.Console.WriteLine("Enter first operand: ");
-            //double First = double.Parse(Console.ReadLine());
-            //System.Console.WriteLine("Enter second operand: ");
-            //double Second = double.Parse(Console.ReadLine());
-            double First = 5.5, Second = 3;
+            System.Console.WriteLine("Enter first operand: ");
+            double First = double.Parse(Console.ReadLine());
+            System.Console.WriteLine("Enter second operand: ");
+            double Second = double.Parse(Console.ReadLine());
             Rezult<double> rez;
-
-            System.Console.WriteLine("Choose operation:\nPush *1* --> to plus operands\nPush *2* --> to minus operands\n Push *3* to find multyplication\nPush *4* --> to divide them\n Your choise:");
+            System.Console.WriteLine("Choose operation:\nPush *1* --> to plus operands\nPush *2* --> to minus operands\nPush *3* to find multyplication\nPush *4* --> to divide them\n Your choise:");
             int choise = int.Parse(Console.ReadLine());
+
             switch (choise)
             {
                 case 1:
@@ -47,14 +46,17 @@ namespace culc
                 break;
                 case 3:
                 rez = new Rezult<double>(Calculator.Multyplication);
-                rez.Invoke(First,Second);
+                Console.WriteLine(rez.Invoke(First,Second));
                 break;
                 case 4:
+                if (Second == 0){
+                    System.Console.WriteLine("Error!");
+                } else {
                 rez = new Rezult<double>(Calculator.Division);
-                rez.Invoke(First,Second);
+                Console.WriteLine(rez.Invoke(First,Second));
+                }                
                 break;
             }
-           
         }
     }
 }
